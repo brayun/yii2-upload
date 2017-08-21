7c484a5fe6c69138c30a815b8d73315726da682e
Ethan <ethan@brayun.com>
2017-08-21 16:09:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+<p align="center">
+    <a href="https://www.brayun.com/" target="_blank">
+        <img src="https://www.brayun.com/img/touch/chrome-touch-icon-192x192.png" width="200" alt="上海柏锐网络科技有限公司" />
+    </a>
+</p>
+
+<p align="center">
+  <a href="https://www.brayun.com">上海柏锐网络科技有限公司</a>是一家技术型服务企业.可提供专业的技术外包服务,定制OA,ERP,电商系统,H5网站,天猫淘宝H5,欢迎前来咨询
+</p>
+
+
+## 安装
+
+```shell
+composer require brayun/yii2-mns
+```
+
+
+## 说明
+  - sdk详细使用请[查看](https://help.aliyun.com/document_detail/51929.html)
---
+++ src/OssFilesystem.php
@@ -0,0 +1,73 @@
+namespace brayun\flysystem;
+
+use creocoder\flysystem\Filesystem;
+use Xxtime\Flysystem\Aliyun\OssAdapter;
+
+/**
+ * OssFilesystem
+ * @author Ethan <ethan@brayun.com>
+ */
+class OssFilesystem extends Filesystem
+{
+    /**
+     * @var string
+     */
+    public $internal = false;
+    /**
+     * @var string
+     */
+    public $ossServer;
+    /**
+     * @var string
+     */
+    public $ossServerInternal;
+    /**
+     * @var string
+     */
+    public $accessKeyId;
+    /**
+     * @var string
+     */
+    public $accessKeySecret;
+    /**
+     * @var string
+     */
+    public $bucket;
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if ($this->accessKeyId === null) {
+            throw new InvalidConfigException('The "accessKeyId" property must be set.');
+        }
+        if ($this->accessKeySecret === null) {
+            throw new InvalidConfigException('The "accessKeySecret" property must be set.');
+        }
+        if ($this->ossServer === null) {
+            throw new InvalidConfigException('The "ossServer" property must be set.');
+        }
+        if ($this->bucket === null) {
+            throw new InvalidConfigException('The "bucket" property must be set.');
+        }
+        if ($this->internal === true) {
+            if ($this->ossServerInternal === null) {
+                throw new InvalidConfigException('The "ossServerInternal" property must be set.');
+            }
+            $this->ossServer = $this->ossServerInternal;
+        }
+        parent::init();
+    }
+    /**
+     * @return DropboxAdapter
+     */
+    protected function prepareAdapter()
+    {
+        return new OssAdapter([
+          'access_id'     => $this->accessKeyId,
+          'access_secret' => $this->accessKeySecret,
+          'bucket'        => $this->bucket,
+          'endpoint'       => $this->ossServers,
+        ]);
+    }
+}
